430f1efd293620d02661d4de5c58cbd624ec3bec
Daniel Henrique de Souza <dhs.danielsouza@gmail.com>
2023-07-10 23:32:36 -0300

    Add routes

---
+++ src/routes/user-routes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router()
+const UserController = require('../controllers/user-controller')
+
+router.get('/', UserController.findAllUsers)
+router.get('/:id', UserController.findUserById)
+router.get('/:email', UserController.findUserByEmail)
+router.post('/', UserController.createUser)
+router.put('/:id', UserController.updateUser)
+router.delete('/:id', UserController.deleteUser)
+
+module.exports = router
